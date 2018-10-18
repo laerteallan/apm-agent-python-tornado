26d8037480a51ec286914541a6d5badcf175dfad
Laerte Allan de Oliveira Araujo <Laerte.araujo@fs.com.br>
2018-10-18 15:58:06 -0300

    Added a file utils.py
    Was added a file utils to functions
    generals

--- src/tornado_elastic/__init__.py
+++ src/tornado_elastic/__init__.py
@@ -1,33 +1,8 @@
 import elasticapm
 import tornado
 from elasticapm.base import Client
-from elasticapm.utils import get_url_dict
 from tornado.web import RequestHandler
-
-
-def get_data_from_request(request):
-    url = '{}://{}{}'.format(request.protocol, request.host, request.uri)
-    data = {
-        "headers": dict(**request.headers),
-        "method": request.method,
-        "socket": {
-            "remote_address": request.remote_ip,
-            "encrypted": request.protocol == 'https'
-        },
-        "cookies": dict(**request.cookies),
-        "url": get_url_dict(url),
-        "body": request.body
-    }
-
-    data["headers"].pop("Cookie", None)
-    return data
-
-
-def get_data_from_response(response):
-    data = {"status_code": response.get_status()}
-    if response._headers:
-        data["headers"] = response._headers._dict
-    return data
+from .utils import get_data_from_request, get_data_from_response
 
 
 def make_client(client_cls, app, **defaults):
